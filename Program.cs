ef3fdc2a6c34291e8a3329dc8d674448799db6ac
João Augusto <jaugusto201618@gmail.com>
2020-12-08 15:13:28 -0300

    Apenas um teste, foi meu primeiro coonato com git

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace proj_teste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("'The Life Snake...'");
+            Console.WriteLine("                      ass: Steve Trabalhos.");            
+
+            
+        }
+    }
+}
